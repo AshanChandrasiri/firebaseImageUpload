41ab723ce390829c5e092759194a8736f2ffc828
AshanChandrasiri <ashanchandrasiri1@gmail.com>
2019-09-30 12:18:34 +0530

    files containing only image upload to firebase

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FirebaseService } from 'src/services/firebase.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'firebase-test';
+  
+}
